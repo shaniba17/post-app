eef277b1ced619a1d47264de38ba831a09ba37cb
shaniba17 <shanibatm2002@gmail.com>
2024-06-08 00:10:27 +0530

    project created

---
+++ src/components/DeletePost.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const DeletePost = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">User Id</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="btn btn-danger">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default DeletePost
