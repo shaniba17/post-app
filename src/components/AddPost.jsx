0f79f4e1d7f5b927533d58371ee4727a59bc5fa3
shaniba17 <shanibatm2002@gmail.com>
2024-06-08 16:03:58 +0530

    reading form values

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavBar from './NavBar'
 
 const AddPost = () => {
+    const [data,setData]=useState(
+    {
+        "userid":"",
+        "id":"",
+        "title":"",
+        "body":""
+
+    }
+    )
+    const inputHandler=(event)=>
+        {
+            setData({...data,[event.target.name]:event.target.value})
+        }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <NavBar/>
@@ -11,22 +27,22 @@ const AddPost = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">User Id</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='userid' value={data.userid} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Id</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='id' value={data.id} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Title</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='title' value={data.title} onChange={inputHandler}/>
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Body</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='body' value={data.body} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Submit</button>
+                            <button className="btn btn-success" onClick={readValue}>Submit</button>
                         </div>
                     </div>
                 </div>
