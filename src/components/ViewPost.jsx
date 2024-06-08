00639e07552e7690e31140ddfc0c0c4c93804f35
shaniba17 <shanibatm2002@gmail.com>
2024-06-08 09:19:31 +0530

    changes added

--- src/components/ViewPost.jsx
+++ src/components/ViewPost.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import NavBar from './NavBar'
 
 const ViewPost = () => {
   const [data, changedata] = useState(
@@ -607,6 +608,7 @@ const ViewPost = () => {
   )
   return (
     <div>
+      <NavBar/>
       <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
